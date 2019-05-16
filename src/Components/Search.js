91a7052079d1baf95b403a3e8d0dfdf8dcf1bc54
Ronald Santos <ronaldsantos63@gmail.com>
2019-05-16 20:09:40 -0300

    fix: when consulting a book was already on a shelf the same was shown as if it were not on any shelf

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import LoadingOverlay from 'react-loading-overlay';
+import {DebounceInput} from 'react-debounce-input';
 
 import BookItem from './BookItem';
 
@@ -51,31 +52,29 @@ class Search extends React.Component {
         BooksAPI.search(search.toLowerCase())
             .then((books) => {
                 if (books.error === undefined){
-                    books = this.checkBookOnShelf(books);
-                    this.setState(() => ({
-                        booksFound: books
-                    }));
+                    this.updateBooks(books);
                 } else {
                     this.clearBooks();
                 }
-
                 this.enableLoading(false);
             });
     }
 
-    checkBookOnShelf = booksFound => {
+    updateBooks = booksFound => {
         const {books} = this.props;
         const newBooks = booksFound.map((b) => {
+            b.shelf = 'none';
             for (let i = 0; i < books.length; i++){
                 if (books[i].id === b.id) {
                     b.shelf = books[i].shelf;
-                } else {
-                    b.shelf = "none";
+                    break
                 }
             }
             return b;
         });
-        return newBooks;
+        this.setState(() => ({
+            booksFound: newBooks
+        }))
     }
 
     render() {
@@ -91,10 +90,10 @@ class Search extends React.Component {
                         Close
                     </Link>
                     <div className="search-books-input-wrapper">
-                    <input 
-                      type="text" 
+                    <DebounceInput 
+                      minLength={2}
+                      debounceTimeout={400}
                       placeholder="Search by title or author"
-                      value={search}
                       onChange={(ev) => this.updateSearch(ev.target.value)}/>
                     </div>
                 </div>
